1114b585726b663e911ea03ea6f77e0a029236fe
SvetaP <svetlanaprosvirina@rambler.ru>
2015-05-09 00:26:55 +0400

    My first java project

--- src/librarydao/AuthorsDAO.java
+++ src/librarydao/AuthorsDAO.java
@@ -12,109 +12,88 @@ public class AuthorsDAO implements GenericDbDAO<Authors> {
 	private static final Logger log = Logger.getLogger(AuthorsDAO.class);
 
 	public void add(Authors entity) {
-		String SQL1 = "INSERT INTO AUTHORS (ID, NAME) "
-				+ "VALUES (my_seq.nextval, ?)";
-		String SQL2 = "SELECT ID FROM AUTHORS WHERE NAME = ?";
-		PreparedStatement pst;
-		ResultSet rs;
-		try {
-			Connection con = Connect.connectionDb();
-			con.setAutoCommit(false);
-			pst = con.prepareStatement(SQL1);
-			pst.setString(1, entity.getName());
-			pst.executeUpdate();
-			con.commit();
-			con.close();
-		} catch (SQLException e) {
-			log.error(e.getMessage(), e);
-		}
-		try {
-			Connection con = Connect.connectionDb();
-			con.setAutoCommit(false);
-			pst = con.prepareStatement(SQL2);
-			pst.setString(1, entity.getName());
-			rs = pst.executeQuery();
+		String SQL1 = "SELECT my_seq.nextval FROM DUAL";
+		String SQL2 = "INSERT INTO AUTHORS (ID, NAME) " + "VALUES (?, ?)";
+		try (Connection con = Connect.connectionDb();
+				Statement st = con.createStatement();) {
+			ResultSet rs = st.executeQuery(SQL1);
 			while (rs.next()) {
-				BigInteger b = BigInteger.valueOf(rs.getLong("ID"));
+				BigInteger b = BigInteger.valueOf(rs.getLong("NEXTVAL"));
 				entity.setId(b);
 			}
-			pst.executeUpdate();
-			rs.close();
-			con.close();
 		} catch (SQLException e) {
 			log.error(e.getMessage(), e);
 		}
+		try (Connection con = Connect.connectionDb();
+				PreparedStatement pst = con.prepareStatement(SQL2);) {
+			con.setAutoCommit(false);
+			pst.setLong(1, entity.getId().longValue());
+			pst.setString(2, entity.getName());
+			pst.executeUpdate();
+			con.commit();
+		} catch (SQLException e) {
+			log.error(e.getMessage(), e);
+		}
+		log.info("Author created");
 	}
 
 	public void delete(BigInteger ID) {
 		String SQL = "DELETE FROM AUTHORS WHERE ID =" + ID;
-		PreparedStatement st;
-		try {
-			Connection con = Connect.connectionDb();
-			st = con.prepareStatement(SQL);
-			st.executeUpdate(SQL);
-			con.close();
+		try (Connection con = Connect.connectionDb();
+				PreparedStatement pst = con.prepareStatement(SQL);) {
+			pst.executeUpdate(SQL);
 		} catch (SQLException e) {
 			log.error(e.getMessage(), e);
 		}
+		log.info("Author deleted");
 	}
 
 	public void update(Authors entity) {
 		String SQL = "UPDATE AUTHORS SET NAME = ? WHERE ID =" + entity.getId();
-		PreparedStatement st;
-		try {
-			Connection con = Connect.connectionDb();
+		try (Connection con = Connect.connectionDb();
+				PreparedStatement pst = con.prepareStatement(SQL);) {
 			con.setAutoCommit(false);
-			st = con.prepareStatement(SQL);
-			st.setString(1, entity.getName());
-			st.executeUpdate(SQL);
+			pst.setString(1, entity.getName());
+			pst.executeUpdate(SQL);
 			con.commit();
-			con.close();
 		} catch (SQLException e) {
 			log.error(e.getMessage(), e);
 		}
+		log.info("Author updated");
 	}
 
 	public List<Authors> getAll() {
 		List<Authors> authors = new ArrayList<Authors>();
 		String SQL = "SELECT NAME FROM AUTHORS";
-		Statement st;
-		ResultSet rs;
-		try {
-			Connection con = Connect.connectionDb();
-			st = con.createStatement();
-			rs = st.executeQuery(SQL);
+		try (Connection con = Connect.connectionDb();
+				Statement st = con.createStatement();) {
+			ResultSet rs = st.executeQuery(SQL);
 			while (rs.next()) {
 				String name = rs.getString("NAME");
 				Authors author = new Authors(name);
 				authors.add(author);
 			}
-			rs.close();
-			st.close();
 		} catch (SQLException e) {
 			log.error(e.getMessage(), e);
 		}
+		log.info("All authors");
 		return authors;
 	}
 
 	public Authors getByID(BigInteger ID) {
 		Authors author = null;
 		String SQL = "SELECT NAME FROM AUTHORS WHERE ID=" + ID;
-		Statement st;
-		ResultSet rs;
-		try {
-			Connection con = Connect.connectionDb();
-			st = con.createStatement();
-			rs = st.executeQuery(SQL);
+		try (Connection con = Connect.connectionDb();
+				Statement st = con.createStatement();) {
+			ResultSet rs = st.executeQuery(SQL);
 			while (rs.next()) {
 				String name = rs.getString("NAME");
 				author = new Authors(name);
 			}
-			rs.close();
-			st.close();
 		} catch (SQLException e) {
 			log.error(e.getMessage(), e);
 		}
+		log.info("Author by ID");
 		return author;
 	}
 
--- src/librarydao/BookDAO.java
+++ src/librarydao/BookDAO.java
@@ -13,116 +13,97 @@ public class BookDAO implements GenericDbDAO<Book> {
 	private static final Logger log = Logger.getLogger(BookDAO.class);
 
 	public void add(Book entity) {
-		String SQL1 = "INSERT INTO BOOK (ID, TITLE, YEAR, PAGES) "
-				+ "VALUES (my_seq.nextval, ?, ?, ?)";
-		String SQL2 = "SELECT ID FROM BOOK WHERE TITLE = ? AND YEAR = ? AND PAGES = ?";
+		String SQL1 = "SELECT my_seq.nextval FROM DUAL";
+		String SQL2 = "INSERT INTO BOOK (ID, TITLE, YEAR, PAGES) "
+				+ "VALUES (?, ?, ?, ?)";
 		String SQL3 = "INSERT INTO BOOKAUTHORS (IDBOOK, IDAUTHORS) "
 				+ "VALUES (?, ?)";
-		PreparedStatement pst;
-		ResultSet rs;
-		try {
-			Connection con = Connect.connectionDb();
-			con.setAutoCommit(false);
-			pst = con.prepareStatement(SQL1);
-			pst.setString(1, entity.getTitle());
-			pst.setInt(2, entity.getYear());
-			pst.setInt(3, entity.getPages());
-			pst.executeUpdate();			
-			con.commit();
-			con.close();
-		} catch (SQLException e) {
-			log.error(e.getMessage(), e);
-		}
-		try {
-			Connection con = Connect.connectionDb();
-			pst = con.prepareStatement(SQL2);
-			pst.setString(1, entity.getTitle());
-			pst.setInt(2, entity.getYear());
-			pst.setInt(3, entity.getPages());
-			rs = pst.executeQuery();
+		try (Connection con = Connect.connectionDb();
+				Statement st = con.createStatement();) {
+			ResultSet rs = st.executeQuery(SQL1);
 			while (rs.next()) {
-				BigInteger b = BigInteger.valueOf(rs.getLong("ID"));
+				BigInteger b = BigInteger.valueOf(rs.getLong("NEXTVAL"));
 				entity.setId(b);
 			}
-			pst.executeUpdate();
-			rs.close();
-			con.close();
 		} catch (SQLException e) {
 			log.error(e.getMessage(), e);
 		}
-		try {
-			Connection con = Connect.connectionDb();
-			pst = con.prepareStatement(SQL3);
-			for(int i=0; i<entity.getAuthors().size(); i++){
+		try (Connection con = Connect.connectionDb();
+				PreparedStatement pst = con.prepareStatement(SQL2);) {
+			con.setAutoCommit(false);
+			pst.setLong(1, entity.getId().longValue());
+			pst.setString(2, entity.getTitle());
+			pst.setInt(3, entity.getYear());
+			pst.setInt(4, entity.getPages());
+			pst.executeUpdate();
+			con.commit();
+		} catch (SQLException e) {
+			log.error(e.getMessage(), e);
+		}
+		try (Connection con = Connect.connectionDb();
+				PreparedStatement pst = con.prepareStatement(SQL3);) {
+			for (int i = 0; i < entity.getAuthors().size(); i++) {
 				con.setAutoCommit(false);
 				pst.setLong(1, entity.getId().longValue());
 				pst.setLong(2, entity.getAuthors().get(i).getId().longValue());
-				pst.executeUpdate();			
+				pst.executeUpdate();
 				con.commit();
 			}
-			con.close();
 		} catch (SQLException e) {
 			log.error(e.getMessage(), e);
-		}		
+		}
+		log.info("Book created");
 	}
 
 	public void delete(BigInteger ID) {
 		String SQL = "DELETE FROM BOOK WHERE ID =" + ID;
-		PreparedStatement st;
-		try {
-			Connection con = Connect.connectionDb();
-			st = con.prepareStatement(SQL);
-			st.executeUpdate(SQL);
-			con.close();
-		} catch (SQLException e) {
-			log.error(e.getMessage(), e);
-		}		
-	}
-
-	public void update(Book entity) {
-		String SQL = "UPDATE BOOK SET TITLE = ? AND YEAR = ? AND PAGES = ? WHERE ID =" + entity.getId();
-		PreparedStatement st;
-		try {
-			Connection con = Connect.connectionDb();
-			con.setAutoCommit(false);
-			st = con.prepareStatement(SQL);
-			st.setString(1, entity.getTitle());
-			st.setInt(2, entity.getYear());
-			st.setInt(3, entity.getPages());
-			st.executeUpdate(SQL);
-			con.commit();
-			con.close();
+		try (Connection con = Connect.connectionDb();
+				PreparedStatement pst = con.prepareStatement(SQL);) {
+			pst.executeUpdate();
 		} catch (SQLException e) {
 			log.error(e.getMessage(), e);
 		}
+		log.info("Book deleted");
+	}
+
+	public void update(Book entity) {
+		String SQL = "UPDATE BOOK SET TITLE = ? AND YEAR = ? AND PAGES = ? WHERE ID ="
+				+ entity.getId();
+		try (Connection con = Connect.connectionDb();
+				PreparedStatement pst = con.prepareStatement(SQL);) {
+			con.setAutoCommit(false);
+			pst.setString(1, entity.getTitle());
+			pst.setInt(2, entity.getYear());
+			pst.setInt(3, entity.getPages());
+			pst.executeUpdate(SQL);
+			con.commit();
+		} catch (SQLException e) {
+			log.error(e.getMessage(), e);
+		}
+		log.info("Book updated");
 	}
 
 	public List<Book> getAll() {
 		List<Book> books = new ArrayList<Book>();
 		List<Authors> authors = null;
 		String SQL1 = "SELECT TITLE, YEAR, PAGES FROM BOOK";
-		String SQL2 = "SELECT NAME FROM AUTHORS T1 INNER JOIN BOOKAUTHORS T2 " 
-				+ "ON T1.ID=T2.IDAUTHORS INNER JOIN BOOK T3 " 
+		String SQL2 = "SELECT NAME FROM AUTHORS T1 INNER JOIN BOOKAUTHORS T2 "
+				+ "ON T1.ID=T2.IDAUTHORS INNER JOIN BOOK T3 "
 				+ "ON T2.IDBOOK=T3.ID WHERE TITLE = ? AND YEAR = ? AND PAGES = ?";
-		Statement st;
-		PreparedStatement pst;
-		ResultSet rs1 = null;
-		ResultSet rs2 = null;
-		try {
-			Connection con = Connect.connectionDb();
-			st = con.createStatement();
-			rs1 = st.executeQuery(SQL1);
+		try (Connection con = Connect.connectionDb();
+				Statement st = con.createStatement();
+				PreparedStatement pst = con.prepareStatement(SQL2);) {
+			ResultSet rs1 = st.executeQuery(SQL1);
 			while (rs1.next()) {
 				String title = rs1.getString("TITLE");
 				Integer year = rs1.getInt("YEAR");
 				Integer pages = rs1.getInt("PAGES");
-				pst = con.prepareStatement(SQL2);
 				pst.setString(1, title);
 				pst.setInt(2, year);
 				pst.setInt(3, pages);
-				rs2 = pst.executeQuery();
+				ResultSet rs2 = pst.executeQuery();
 				authors = new ArrayList<Authors>();
-				while(rs2.next()){
+				while (rs2.next()) {
 					String name = rs2.getString("NAME");
 					Authors author = new Authors(name);
 					authors.add(author);
@@ -130,13 +111,10 @@ public class BookDAO implements GenericDbDAO<Book> {
 				Book book = new Book(authors, title, year, pages);
 				books.add(book);
 			}
-			rs1.close();
-			rs2.close();
-			st.close();
-			con.close();
 		} catch (SQLException e) {
 			log.error(e.getMessage(), e);
 		}
+		log.info("All books");
 		return books;
 	}
 
@@ -146,31 +124,26 @@ public class BookDAO implements GenericDbDAO<Book> {
 		String SQL1 = "SELECT TITLE, YEAR, PAGES FROM BOOK WHERE ID=" + ID;
 		String SQL2 = "SELECT NAME FROM AUTHORS T1 FULL JOIN BOOKAUTHORS T2"
 				+ "ON T1.ID=T2.IDAUTHORS WHERE IDBOOK=" + ID;
-		Statement st;
-		ResultSet rs1 = null;
-		ResultSet rs2 = null;
-		try {
-			Connection con = Connect.connectionDb();
-			st = con.createStatement();
-			rs1 = st.executeQuery(SQL1);
+		try (Connection con = Connect.connectionDb();
+				Statement st = con.createStatement();) {
+			ResultSet rs1 = st.executeQuery(SQL1);
 			while (rs1.next()) {
 				String title = rs1.getString("TITLE");
 				Integer year = rs1.getInt("YEAR");
 				Integer pages = rs1.getInt("PAGES");
-				rs2 = st.executeQuery(SQL2);
-				while(rs2.next()){
+				ResultSet rs2 = st.executeQuery(SQL2);
+				while (rs2.next()) {
 					String name = rs2.getString("NAME");
 					Authors author = new Authors(name);
 					authors.add(author);
 				}
 				book = new Book(authors, title, year, pages);
 			}
-			rs1.close();
-			st.close();
 		} catch (SQLException e) {
 			log.error(e.getMessage(), e);
 		}
+		log.info("Book by ID");
 		return book;
 	}
-	
+
 }
